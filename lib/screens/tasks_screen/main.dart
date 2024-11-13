f9a887d4cefd4f64d130821aa8a3b3fcfc848d58
Ashley <ashley.liz2605@gmail.com>
2024-11-12 19:03:58 -0600

    Organized imports

--- lib/screens/tasks_screen/main.dart
+++ lib/screens/tasks_screen/main.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
+import 'package:flutter/material.dart';
 
 import 'package:day_pal/core/repositories/task_repository.dart';
+import 'package:day_pal/core/entities/task.dart';
 import 'package:day_pal/database_context.dart';
 import 'package:day_pal/screens/tasks_screen/task_form/main.dart';
-import 'package:flutter/material.dart';
-import 'package:day_pal/core/entities/task.dart';
 import 'package:day_pal/screens/tasks_screen/task_item.dart';
 
 class TasksScreen extends StatefulWidget {
